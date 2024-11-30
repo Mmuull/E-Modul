ce87ec0d5968d42b7f99100e5af4ffdba75a4eee
Mmuull <mulky.acmilan625@gmail.com>
2024-11-30 14:33:24 +0700

    main

---
+++ admin/cptp/capaianpembelajaran.php
@@ -0,0 +1,73 @@
+<section class="content-header">
+	<h1 style="text-align:center;">
+		Capaian Pembelajaran
+	</h1>
+	<ol class="breadcrumb">
+		<li>
+			<a href="index.php">
+				<i class="fa fa-home"></i>
+				<b>E-Modul</b>
+			</a>
+		</li>
+	</ol>
+</section>
+<!-- Main content -->
+<section class="content">
+	<div class="box box-primary">
+		<!-- /.box-header -->
+		<div class="box-body">
+			<p style="font-size: 16px; text-align: justify">
+				Pada akhir Fase D, peserta didik mampu menerapkan proses berpikir efektif dan efisien untuk menyelesaikan persoalan berlatar belakang informatika yang didasari data terstruktur, menentukan langkah-langkah untuk mengolah instruksi interaktif dan data yang dapat dijalankan oleh manusia atau mesin otomasi, dan memanfaatkan dengan optimal sekumpulan fasilitas yang tersedia, memanfaatkan data internet, serta mampu bersosialisasi dan berkolaborasi dengan baik, serta berkreasi dengan etis di dunia digital. 
+			</p>
+			<br>
+			<p style="font-size: 16px; text-align: justify">
+				Peserta didik mampu memahami cara kerja dan penggunaan mesin pencari di internet; mengetahui kredibilitas sumber informasi digital dan mengenal ekosistem media pers digital; membedakan fakta dan opini; memahami pemanfaatan perkakas teknologi digital untuk membuat laporan, presentasi, serta analisis dan interpretasi data; mampu mendeskripsikan komponen, fungsi, dan cara kerja komputer; memahami konsep dan penerapan konektivitas jaringan lokal dan internet baik kabel maupun nirkabel; dan mengetahui jenis ruang publik virtual; memahami pemanfaatan media digital untuk produksi dan diseminasi konten. Peserta didik mampu memahami pentingnya menjaga rekam jejak digital, mengamalkan toleransi dan empati di dunia digital, memahami dampak perundungan digital, membuat kata sandi yang aman, memahami pengamanan perangkat dari berbagai jenis malware, memilah informasi yang bersifat privat dan publik, melindungi data pribadi dan identitas digital, serta memiliki kesadaran penuh (mindfulness) dalam dunia digital.
+			</p>
+			<!-- <div class="table-responsive">
+			
+				<table id="example1" class="table table-bordered table-striped">
+					<thead>
+						<tr>
+							<th>No</th>
+							<th>Buku</th>
+							<th>Peminjam</th>
+							<th>Tgl Di kembalikan</th>
+						</tr>
+					</thead>
+					<tbody>
+
+						<?php /*
+                  $no = 1;
+                  $sql = $koneksi->query("SELECT b.judul_buku, a.id_anggota, a.nama, s.tgl_kembali, s.tgl_dikembalikan
+                  from tb_sirkulasi s inner join tb_buku b on s.id_buku=b.id_buku
+				  inner join tb_anggota a on s.id_anggota=a.id_anggota where status='KEM' order by tgl_kembali asc");
+                  while ($data= $sql->fetch_assoc()) {
+                ?>
+
+						<tr>
+							<td>
+								<?php echo $no++; ?>
+							</td>
+							<td>
+								<?php echo $data['judul_buku']; ?>
+							</td>
+							<td>
+								<?php echo $data['id_anggota']; ?>
+								-
+								<?php echo $data['nama']; ?>
+							</td>
+							<td>
+							<?php  $tgl = $data['tgl_dikembalikan']; echo date("d/M/Y", strtotime($tgl))?>
+							</td>
+						</tr>
+						<?php
+                  } */
+                ?>
+					</tbody>
+
+				</table>
+			</div> -->
+		</div>
+	</div>
+</section>
+
---
+++ admin/cptp/tujuanpembelajaran.php
@@ -0,0 +1,73 @@
+<section class="content-header">
+	<h1 style="text-align:center;">
+		Tujuan Pembelajaran
+	</h1>
+	<ol class="breadcrumb">
+		<li>
+			<a href="index.php">
+				<i class="fa fa-home"></i>
+				<b>E-Modul</b>
+			</a>
+		</li>
+	</ol>
+</section>
+<!-- Main content -->
+<section class="content">
+	<div class="box box-primary">
+		<!-- /.box-header -->
+		<div class="box-body">
+			<p style="font-size: 16px; text-align: justify">
+			Setelah mempelajari materi ini peserta didik mampu: <br>
+			1.Mengidentifikasi spesifikasi perangkat keras komputer. <br>
+			2.Menjelaskan fungsi fungsi perangkat keras komputer. <br>
+			3.Menjelaskan jenis jenis perangkat keras komputer. <br>
+			4.Menganalisis perangkat keras komputer. <br>
+			</p>
+			<!-- <div class="table-responsive">
+			
+				<table id="example1" class="table table-bordered table-striped">
+					<thead>
+						<tr>
+							<th>No</th>
+							<th>Buku</th>
+							<th>Peminjam</th>
+							<th>Tgl Di kembalikan</th>
+						</tr>
+					</thead>
+					<tbody>
+
+						<?php /*
+                  $no = 1;
+                  $sql = $koneksi->query("SELECT b.judul_buku, a.id_anggota, a.nama, s.tgl_kembali, s.tgl_dikembalikan
+                  from tb_sirkulasi s inner join tb_buku b on s.id_buku=b.id_buku
+				  inner join tb_anggota a on s.id_anggota=a.id_anggota where status='KEM' order by tgl_kembali asc");
+                  while ($data= $sql->fetch_assoc()) {
+                ?>
+
+						<tr>
+							<td>
+								<?php echo $no++; ?>
+							</td>
+							<td>
+								<?php echo $data['judul_buku']; ?>
+							</td>
+							<td>
+								<?php echo $data['id_anggota']; ?>
+								-
+								<?php echo $data['nama']; ?>
+							</td>
+							<td>
+							<?php  $tgl = $data['tgl_dikembalikan']; echo date("d/M/Y", strtotime($tgl))?>
+							</td>
+						</tr>
+						<?php
+                  } */
+                ?>
+					</tbody>
+
+				</table>
+			</div> -->
+		</div>
+	</div>
+</section>
+
